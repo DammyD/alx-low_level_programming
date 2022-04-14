660f82acc335a108a8b57439d32de2f4fa8dc3bf
DammyD <ibukunoluwadawodu@gmail.com>
2022-04-14 05:57:56 -0700

    Write a function that prints 10 times the numbers.

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,7 +7,7 @@
 void more_numbers(void)
 {
 int i;
-int first_number:
+int first_number;
 int second_number;
 int result;
 
@@ -20,6 +20,7 @@ while (result <= 14)
 if (result < 10)
 {
 second_number = result;
+
 }
 else
 {
