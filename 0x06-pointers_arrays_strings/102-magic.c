3fe13e60dd91a85f03d8a5c00361978bf4ca9b59
DammyD <ibukunoluwadawodu@gmail.com>
2022-04-24 11:36:02 -0700

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,22 +1,24 @@
 #include <stdio.h>
-/**
- * Instructions:
- * You are not allowed to use the var a
- * You are not allowed to modify the var p
- * You can only write one statement
- * You are not allowed to code anything else but the line of code
- * Return: Always 0
- */
+
 int main(void)
 {
-
 int n;
 int a[5];
 int *p;
 
 a[2] = 1024;
 p = &n;
+/**
+ * write your line of code here...
+ * Instructions:
+ * You are not allowed to use the var a
+ * You are not allowed to modify the var p
+ * You can only write one statement
+ * You are not allowed to code anything else but the line of code
+ */
 *(p + 5) = 98;
+/*...so that this prints 98\n */
 printf("a[2] = %d\n", a[2]);
+
 return (0);
 }
