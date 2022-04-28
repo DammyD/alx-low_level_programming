b39d8d4558145e7400005d57bc0e9de206d56634
DammyD <ibukunoluwadawodu@gmail.com>
2022-04-28 15:22:56 -0700

    Write a function that compares two strings.

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,29 +1,22 @@
 #include "main.h"
 /**
- * wildcmp - compares two strings
- * @s1: pointer to a string params
- * @s2: pointer to a string params
+ * wildcmp - compares two string
+ * @s1: pointer to string params
+ * @s2: pointer to string params
  * Return: 0
  */
 int wildcmp(char *s1, char *s2)
 {
-
-if (*s1 == '\0')
-{
-if (*s2 != '\0' && *s2 == '*')
-{
-return (wildcmp(s1, s2 + 1));
-}
-return (*s2 == '\0');
-}
+if (*s1 == '\0' && *s2 == '\0')
+return (1);
+if (*s1 == *s2)
+return (wildcmp(s1 + 1, s2 + 1));
 if (*s2 == '*')
 {
-return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1);
+if (*(s2 + 1) == '*')
+return (wildcmp(s1, s2 + 1));
+if (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1))
+return (1);
 }
-else if (*s1 == *s2)
-{
-return (wildcmp(s1 + 1, s2 + 1));
-}
-
 return (0);
 }
