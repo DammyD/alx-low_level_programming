212a7b28df2020a3b02957f85475059b6d00a5e5
DammyD <ibukunoluwadawodu@gmail.com>
2022-05-05 16:54:28 -0700

    Write a function that concatenates all the arguments of your program.

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -12,38 +12,39 @@
 
 char *argstostr(int ac, char **av)
 {
-	char *s;
-	int l, lt, i, j, k;
-	l = 0; k = 0; lt = 0;
+char *p;
+int l, lt, i, j, k;
 
-	if (ac == 0 || av == NULL)
-		return (0);
+if (ac == 0 || av == NULL)
+return (0);
+l = 0; k = 0;
 
-	for (i = 0; i < ac; i++)
-	{
+for (i = 0; i < ac; i++)
+{
+lt = 0;
 
-		while (av[i][lt])
-			lt++;
-		l += lt + 1;
-	}
-	s = malloc((l + 1) * sizeof(char));
+while (av[i][lt])
+lt++;
+l += lt + 1;
+}
+p = malloc((l + 1) * sizeof(char));
 
-	if (s == 0)
-		return (0);
+if (p == 0)
+return (0);
 
-	for (j = 0; j < ac; j++)
-	{
+for (j = 0; j < ac; j++)
+{
+lt = 0;
+while (av[j][lt])
+{
+*(p + k) = av[j][lt];
+k++;
+lt++;
+}
+*(p + k) = '\n';
+k++;
+}
+*(p + k) = '\0';
 
-		while (av[j][lt])
-		{
-			*(s + k) = av[j][lt];
-			k++;
-			lt++;
-		}
-		*(s + k) = '\n';
-		k++;
-	}
-	*(s + k) = '\0';
-
-	return (s);
+return (p);
 }
