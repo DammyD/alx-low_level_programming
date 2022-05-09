7dab703eb5f5424c408bb551b9db540e5e1d17a3
DammyD <ibukunoluwadawodu@gmail.com>
2022-05-09 05:37:37 -0700

    Write a function that concatenates two strings.

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,36 +12,36 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int l1, l2, i, j;
-	char *p;
-	char *nul = "";
+unsigned int l1, l2, i, j;
+char *p;
+char *nul = "";
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
+if (s1 == NULL)
+s1 = nul;
+if (s2 == NULL)
+s2 = nul;
 
-	l1 = 0, l2 = 0;
-	while (*(s1 + l1))
-		l1++;
-	while (*(s2 + l2))
-		l2++;
+l1 = 0, l2 = 0;
+while (*(s1 + l1))
+l1++;
+while (*(s2 + l2))
+l2++;
 
-	if (n < l2)
-		l2 = n;
+if (n < l2)
+l2 = n;
 
-	p = malloc(sizeof(char) * (l1 + l2 + 1));
+p = malloc(sizeof(char) * (l1 + l2 + 1));
 
-	if (p == NULL)
-		return (0);
+if (p == NULL)
+return (0);
 
-	for (i = 0; i < l1; i++)
-		p[i] = s1[i];
+for (i = 0; i < l1; i++)
+*(p + i) = *(s1 + i);
 
-	for (i = 0, j = l1; i < l2; j++, i++)
-		p[j] = s2[i];
+for (i = 0, j = l1; i < l2; j++, i++)
+*(p + j) = *(s2 + i);
 
-	p[j] = '\0';
+*(p + j) = '\0';
 
-	return (p);
+return (p);
 }
