62bfc22caa432774aa42ab6edf28dd35949636f9
DammyD <ibukunoluwadawodu@gmail.com>
2022-05-24 05:04:43 -0700

    Write a function that prints all the elements.

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <lists.h>
+/**
+ * print_list - prints all the linked list elements
+ * @h: the head of linked list
+ * Return: the number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t n = 0;
+	len;
+	char *s;
+	const list_t *temp = h;
+
+	while (temp != NULL)
+	{
+		s = temp->str;
+		if (s == NULL)
+		{
+			printf("[o] (nil)\n");
+			temp = temp->next;
+			n++;
+			continue;
+		}
+		len = temp->len;
+		printf("[%u] %s\n", len, s);
+		temp = temp->next
+			n++;
+	}
+	return(n);
+}
