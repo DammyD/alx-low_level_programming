c89ef4d19717403e0699021d2261bf8d28ca16e1
DammyD <ibukunoluwadawodu@gmail.com>
2022-05-24 08:16:27 -0700

    Write a function that frees a list.

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -8,11 +8,12 @@
 void free_list(list_t *head)
 {
 	list_t *temp;
+
 	while (head != NULL)
 	{
 		temp = head;
 		temp = head->next;
-		free(head->str);
-		free(head);
+		free(temp->str);
+		free(temp);
 	}
 }
