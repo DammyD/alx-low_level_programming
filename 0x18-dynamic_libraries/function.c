bbad95760c2f6e68743aa33f4d1e62f07753c3dd
DammyD <ibukunoluwadawodu@gmail.com>
2022-07-12 14:24:08 -0700

    Create a dynamic function that contains C functions

---
+++ 0x18-dynamic_libraries/function.c
@@ -0,0 +1,61 @@
+#include "main.h"
+
+/**
+ * sum - addition operator
+ * @a: first int
+ * @b: second int
+ * Return: sum of 2 numbers
+ */
+
+int sum(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtraction operator
+ * @a: first int
+ * @b: secong int
+ * Return: difference of 2 numbers
+ */
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiplication operator
+ * @a: first int
+ * @b: second int
+ * Return: multiplication of 2 numbers
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - division operator
+ * @a: first int
+ * @b: second int
+ * Return: division of 2 numbers
+ */
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - modulus operator
+ * @a: first int
+ * @b: second int
+ * Return: reminder of division of 2 numbers
+ */
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
